a190d0fe04356474825f84dd87f75fed8aa500f2
Meep the Furry <100735838+sadblooketgamer@users.noreply.github.com>
2024-09-08 19:05:41 -0400

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,16 @@
+#allows to import to everything else
+class Stats:
+  def __init__(self):
+    self.Name=""
+    self.Weapon=""
+    self.Place=""
+    self.Armor=0
+    self.HP=100
+    self.maxHP=100
+    self.Lev=0
+    self.xp=0
+    self.Attack=0
+    self.CritChance=0
+    self.spotsHeal=0
+    self.lpotsHeal=0
+    self.gold=0
